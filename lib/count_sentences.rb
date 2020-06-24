2deb5a6916b87f54dc19f7cf8e0ee3fad2b90a97
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-24 23:19:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
+    #returns true : boolean 
+    #if ends in a . [last], #end_with?(".")
+    #use self keyword 
+    self.end_with?(".")
+    
+    ##for fun try : turning string to array, index last item, 
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # if end with ". || ? || ! " = end of sentence - split here 
+    #if self.end_with?("."||"?"||"!")
+       
+       #this will (.split) return an array ; 
+       #we want to delete array item if less than 2 
+       #"Hey. I like soup!! Just kidding!?!" >> 
+       
+       self.split(/\.|\?|\!/).delete_if do |counter|
+          counter.length < 2 
+       end.count  
+       
+       #return sentence_split.count 
+     #else 
+       #return 0 
+     #end 
   end
 end
