17a1107eac6eda4771d359c2c3d480fcf17b565a
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-24 22:32:39 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
+    #returns true : boolean 
+    #if ends in a . [last], #end_with?(".")
+    #use self keyword 
+    self.end_with?(".")
+    
+    ##for fun try : turning string to array, index last item, 
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # if end with ". || ? || ! " = end of sentence - split here 
+    #if self.end_with?("."||"?"||"!")
+       sentence_split = self.split("."||"?"||"!")
+       #entence_complex = self.end_with?("?", "!", ".").split("?", "!", ".")
+       return sentence_split.count 
+     #else 
+       #return 0 
+     #end 
   end
 end
